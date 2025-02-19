7cd9cb426f8fcff1594597e221f3603ec474e1f9
Denis Ivanov <164205058+Zeshinn@users.noreply.github.com>
2025-02-19 16:14:54 +0200

    Hello World

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+const cors = require("cors");
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+    cors: {
+        origin: "https://zeshinn.github.io/chatroom-client/",
+        methods: ["GET", "POST"]
+    }
+});
+
+app.use(cors());
+
+io.on("connection", (socket) => {
+    console.log("A user connected:", socket.id);
+
+    socket.on("chat message", (data) => {
+        io.emit("chat message", data);  
+    });
+
+    socket.on("disconnect", () => {
+        console.log("A user disconnected:", socket.id);
+    });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
