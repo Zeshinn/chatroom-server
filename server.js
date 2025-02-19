41189e406724a972b0b447c83665c4307be837eb
Denis Ivanov <164205058+Zeshinn@users.noreply.github.com>
2025-02-19 16:47:41 +0200

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: "https://zeshinn.github.io/chatroom-client/",
+        origin: "https://zeshinn.github.io",
         methods: ["GET", "POST"]
     }
 });
